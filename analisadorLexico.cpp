4088c71da6c3f3147be2331528ae2b9444fa7a83
Bryan <bryans0701@gmail.com>
2022-10-01 19:37:26 -0300

    começando a implementação do autômato

--- analisadorLexico.cpp
+++ analisadorLexico.cpp
@@ -1,178 +1,219 @@
+/**
+ * Trabalho Prático de Compiladores
+ * Parte 1 - Analisador Léxico
+ * 
+ * @authors Bryan Santos e Igor Reis - 02/2022
+ */
+
 #include<iostream>
 #include<string>
 #include<unordered_map>
 #include<vector>
-// #include<tuple>
 
 using namespace std;
 
-#define tk_const            0
-#define tk_int              1
-#define tk_char             2
-#define tk_while            3
-#define tk_if               4
-#define tk_float            5
-#define tk_else             6
-#define tk_and              7
-#define tk_or               8
-#define tk_not              9
-#define tk_atrib            10
-#define tk_igualdade        11
-#define tk_abreParentese    12
-#define tk_fechaParentese   13
-#define tk_menor            14
-#define tk_maior            15
-#define tk_diferente        16
-#define tk_maiorIgual       17
-#define tk_menorIgual       18
-#define tk_virgula          19
-#define tk_hifen            20
-#define tk_asterisco        21
-#define tk_barra            22
-#define tk_pontoevirgula    23
-#define tk_abreChave        24
-#define tk_fechaChave       25
-#define tk_readln           26
-#define tk_div              27
-#define tk_string           28
-#define tk_write            29
-#define tk_writeln          30
-#define tk_mod              31
-#define tk_abreColchete     32
-#define tk_fechaColchete    33
-#define tk_true             34
-#define tk_false            35
-#define tk_boolean          36
-
-// typedef vector< tuple<string,string> > tuple_list;
-
-// struct Token
-// {
-//     int token;
-// };
+/** Definindo e Enumerando os tokens */
+#define TK_const            0
+#define TK_int              1
+#define TK_char             2
+#define TK_while            3
+#define TK_if               4
+#define TK_float            5
+#define TK_else             6
+#define TK_and              7
+#define TK_or               8
+#define TK_not              9
+#define TK_atrib            10
+#define TK_igualdade        11
+#define TK_abreParentese    12
+#define TK_fechaParentese   13
+#define TK_menor            14
+#define TK_maior            15
+#define TK_diferente        16
+#define TK_maiorIgual       17
+#define TK_menorIgual       18
+#define TK_virgula          19
+#define TK_hifen            20
+#define TK_asterisco        21
+#define TK_barra            22
+#define TK_pontoevirgula    23
+#define TK_abreChave        24
+#define TK_fechaChave       25
+#define TK_readln           26
+#define TK_div              27
+#define TK_string           28
+#define TK_write            29
+#define TK_writeln          30
+#define TK_mod              31
+#define TK_abreColchete     32
+#define TK_fechaColchete    33
+#define TK_true             34
+#define TK_false            35
+#define TK_boolean          36
 
 
-class Tabela_simbolos{
+/** Tabela de símbolos, armazenando identificadores e palavras reservadas da linguagem */
+class Tabela_simbolos {
     public:
-        // tabela hash que mapeia lexema para token
+        /** Tabela hash que mapeia lexema para token */
         unordered_map<string, int> tab_simbolos; 
 
-        Tabela_simbolos(); // construtor
+        Tabela_simbolos();
         int inserir(string, int);
 
-    // insere as palavras reservadas na tabela de simbolos
-    Tabela_simbolos()
-    {
-        tab_simbolos.insert(make_pair("const" , tk_const));
-        tab_simbolos.insert(make_pair("int" , tk_int));
-        tab_simbolos.insert(make_pair("char" , tk_char));
-        tab_simbolos.insert(make_pair("while" , tk_while));
-        tab_simbolos.insert(make_pair("if" , tk_if));
-        tab_simbolos.insert(make_pair("float" , tk_float));
-        tab_simbolos.insert(make_pair("else" , tk_else));
-        tab_simbolos.insert(make_pair("&&" , tk_and));
-        tab_simbolos.insert(make_pair("||" , tk_or));
-        tab_simbolos.insert(make_pair("!" , tk_not));
-        tab_simbolos.insert(make_pair(":=" , tk_atrib));
-        tab_simbolos.insert(make_pair("=" , tk_igualdade));
-        tab_simbolos.insert(make_pair("(" , tk_abreParentese));
-        tab_simbolos.insert(make_pair(")" , tk_fechaParentese));
-        tab_simbolos.insert(make_pair("<" , tk_menor));
-        tab_simbolos.insert(make_pair(">" , tk_maior));
-        tab_simbolos.insert(make_pair("!=" , tk_diferente));
-        tab_simbolos.insert(make_pair(">=" , tk_maiorIgual));
-        tab_simbolos.insert(make_pair("<=" , tk_menorIgual));
-        tab_simbolos.insert(make_pair("," , tk_virgula));
-        tab_simbolos.insert(make_pair("-" , tk_hifen));
-        tab_simbolos.insert(make_pair("*" , tk_asterisco));
-        tab_simbolos.insert(make_pair("/" , tk_barra));
-        tab_simbolos.insert(make_pair(";" , tk_pontoevirgula));
-        tab_simbolos.insert(make_pair("{" , tk_abreChave));
-        tab_simbolos.insert(make_pair("}" , tk_fechaChave));
-        tab_simbolos.insert(make_pair("readln" , tk_readln));
-        tab_simbolos.insert(make_pair("div" , tk_div));
-        tab_simbolos.insert(make_pair("string" , tk_string));
-        tab_simbolos.insert(make_pair("write" , tk_write));
-        tab_simbolos.insert(make_pair("writeln" , tk_writeln));
-        tab_simbolos.insert(make_pair("mod" , tk_mod));
-        tab_simbolos.insert(make_pair("[" , tk_abreColchete));
-        tab_simbolos.insert(make_pair("]" , tk_fechaColchete));
-        tab_simbolos.insert(make_pair("true" , tk_true));
-        tab_simbolos.insert(make_pair("false" , tk_false));
-        tab_simbolos.insert(make_pair("boolean" , tk_boolean));
+        /** Construtor: Insere as palavras reservadas na tabela de simbolos */
+        Tabela_simbolos() {
+            tab_simbolos.insert(make_pair("const" , TK_const));
+            tab_simbolos.insert(make_pair("int" , TK_int));
+            tab_simbolos.insert(make_pair("char" , TK_char));
+            tab_simbolos.insert(make_pair("while" , TK_while));
+            tab_simbolos.insert(make_pair("if" , TK_if));
+            tab_simbolos.insert(make_pair("float" , TK_float));
+            tab_simbolos.insert(make_pair("else" , TK_else));
+            tab_simbolos.insert(make_pair("&&" , TK_and));
+            tab_simbolos.insert(make_pair("||" , TK_or));
+            tab_simbolos.insert(make_pair("!" , TK_not));
+            tab_simbolos.insert(make_pair(":=" , TK_atrib));
+            tab_simbolos.insert(make_pair("=" , TK_igualdade));
+            tab_simbolos.insert(make_pair("(" , TK_abreParentese));
+            tab_simbolos.insert(make_pair(")" , TK_fechaParentese));
+            tab_simbolos.insert(make_pair("<" , TK_menor));
+            tab_simbolos.insert(make_pair(">" , TK_maior));
+            tab_simbolos.insert(make_pair("!=" , TK_diferente));
+            tab_simbolos.insert(make_pair(">=" , TK_maiorIgual));
+            tab_simbolos.insert(make_pair("<=" , TK_menorIgual));
+            tab_simbolos.insert(make_pair("," , TK_virgula));
+            tab_simbolos.insert(make_pair("-" , TK_hifen));
+            tab_simbolos.insert(make_pair("*" , TK_asterisco));
+            tab_simbolos.insert(make_pair("/" , TK_barra));
+            tab_simbolos.insert(make_pair(";" , TK_pontoevirgula));
+            tab_simbolos.insert(make_pair("{" , TK_abreChave));
+            tab_simbolos.insert(make_pair("}" , TK_fechaChave));
+            tab_simbolos.insert(make_pair("readln" , TK_readln));
+            tab_simbolos.insert(make_pair("div" , TK_div));
+            tab_simbolos.insert(make_pair("string" , TK_string));
+            tab_simbolos.insert(make_pair("write" , TK_write));
+            tab_simbolos.insert(make_pair("writeln" , TK_writeln));
+            tab_simbolos.insert(make_pair("mod" , TK_mod));
+            tab_simbolos.insert(make_pair("[" , TK_abreColchete));
+            tab_simbolos.insert(make_pair("]" , TK_fechaColchete));
+            tab_simbolos.insert(make_pair("true" , TK_true));
+            tab_simbolos.insert(make_pair("false" , TK_false));
+            tab_simbolos.insert(make_pair("boolean" , TK_boolean));
+        }
 
-        // inserir("const" , tk_const);
-        // inserir("int" , tk_int);
-        // inserir("char" , tk_char);
-        // inserir("while" , tk_while);
-        // inserir("if" , tk_if);
-        // inserir("float" , tk_float);
-        // inserir("else" , tk_else);
-        // inserir("&&" , tk_and);
-        // inserir("||" , tk_or);
-        // inserir("!" , tk_not);
-        // inserir(":=" , tk_atrib);
-        // inserir("=" , tk_igualdade);
-        // inserir("(" , tk_abreParentese);
-        // inserir(")" , tk_fechaParentese);
-        // inserir("<" , tk_menor);
-        // inserir(">" , tk_maior);
-        // inserir("!=" , tk_diferente);
-        // inserir(">=" , tk_maiorIgual);
-        // inserir("<=" , tk_menorIgual);
-        // inserir("," , tk_virgula);
-        // inserir("-" , tk_hifen);
-        // inserir("*" , tk_asterisco);
-        // inserir("/" , tk_barra);
-        // inserir(";" , tk_pontoevirgula);
-        // inserir("{" , tk_abreChave);
-        // inserir("}" , tk_fechaChave);
-        // inserir("readln" , tk_readln);
-        // inserir("div" , tk_div);
-        // inserir("string" , tk_string);
-        // inserir("write" , tk_write);
-        // inserir("writeln" , tk_writeln);
-        // inserir("mod" , tk_mod);
-        // inserir("[" , tk_abreColchete);
-        // inserir("]" , tk_fechaColchete);
-        // inserir("true" , tk_true);
-        // inserir("false" , tk_false);
-        // inserir("boolean" , tk_boolean);
-    }
-
-    /*return pesquisar(string lexema)
-    {
-
-    }*/
-    
-
+        /** Verifica se um lexema já existe na tabela */
+        string pesquisar(string lexema);
 
 };
 
-string lexema;
-int numLinhas = 0;
 
-// AUTOMATO
-string getNextToken(string lexema)
-{
-    int estado_atual = 0;
-    int estado_final = (?);
-    string lexema = "";
-    char c;
+/** Verifica se o caracter lido é válido */
+bool isValidChar(char c) {}
+
+
+/** Verifica se o caracter lido é uma letra */
+bool isLetter(char c) {}
+
+
+/** Verifica se o caracter lido é um número */
+bool isNumber(char c) {}
+
+
+/** Implementando o autômato (analisador léxico) */
+string getNextToken(char c) {
+    int estado_atual = 0, estado_final = 2;
+    string lex = "";
     
-    //SWITCH gigante
+    while (estado_atual != estado_final) {
+        if (isValidChar(c)) {
+            switch (estado_atual) {
+            
+                /** Estado Inicial **/
+                case 0:
+
+                    // Caracter em branco
+                    if (c == ' ');
+
+                    // Letra ou _ levam a um identificador ou palavra reservada
+                    else if (isLetter(c) || c == '_') { lex = c; estado_atual = 1; }
+
+                    // " (leva a uma string)
+                    else if (c == '\"') { lex = c; estado_atual = 3; }
+
+                    // Um dígito
+                    else if (isNumber(c)) { lex = c; estado_atual = 4; }
+
+                    // Ponto (leva a um número real ".456")
+                    else if (c == '.') { lex = c; estado_atual = 6; }
+
+                    // >, < ou ! (Pode vir seguido de = ou não)
+                    else if (c == '>' || c == '<' || c == '!') { lex = c; estado_atual = 7; }
+
+                    // & (AND)
+                    else if (c == '&') { lex = c; estado_atual = 8; }
+
+                    // | (OR)
+                    else if (c == '|') { lex = c; estado_atual = 9; }
+
+                    // : (vem seguido de '=' -> atribuição)
+                    else if (c == ':') { lex = c; estado_atual = 10; }
+
+                    // / (início de comentário)
+                    else if (c == '/') { lex = c; estado_atual = 11; }
+
+                    // ' (leva a um char)
+                    else if (c == '\'') { lex = c; estado_atual = 12; }
+
+                    // Caracteres independentes (+, -, ;, (), [], {}, etc...)
+                    else {
+                        lex = c;
+                        estado_atual = estado_final;
+                    }
+                    break;
+
+                // Letra, Dígito ou sublinhado
+                case 1:
+                    while(isLetter(c) || isNumber(c) || c == '_') {
+                        lex += c;
+                        cin >> c;
+                    }
+                    // > Devolve o caracter lido
+                    // > Pesquisa o lex na tabela de simbolos
+                    estado_atual = estado_final;
+                    break;
+
+                // Conteúdo da string até o fechamento das aspas
+                case 3:
+                    while (c != '\"' && c != '\n') {
+                        lex += c;
+                        cin >> c;
+                    }
+                    if (c == '\"') {
+                        lex += c;
+                        estado_atual = estado_final;
+                    }
+                    break;
+
+                // Constante numérica
+                case 4: 
+                
+                    break;
+            }
+
+        }
+
+        // Lendo o próximo caracter
+        cin >> c;
+    }
 }
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
     Tabela_simbolos tab_simbolos;
+    char c;
 
-    while (cin >> lexema)
-    {
-        
-        getNextToken(lexema);
-    }
-
+    // Ler enquanto não chegar ao final do arquivo
+    while (cin >> c) getNextToken(c);
 
     return 0;
 }
